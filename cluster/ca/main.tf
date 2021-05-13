dbe6d40cd57116ae29f97c67ad57abb775e572af
Petr Motejlek <petr@motejlek.net>
2021-05-13 18:32:33 +0200

    create a CA and provision it locally on the "host" machine

---
+++ cluster/ca/main.tf
@@ -0,0 +1,54 @@
+resource "tls_private_key" "ca" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "tls_self_signed_cert" "ca" {
+  key_algorithm = tls_private_key.ca.algorithm
+  private_key_pem = tls_private_key.ca.private_key_pem
+
+  subject {
+    common_name = "ca"
+  }
+
+  validity_period_hours = 48
+
+  is_ca_certificate = true
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing"
+  ]
+}
+
+resource "local_file" "ca" {
+  content = tls_self_signed_cert.ca.cert_pem
+  filename = "ca.pem"
+}
+
+resource "null_resource" "ca" {
+  depends_on = [
+    local_file.ca]
+
+  provisioner "local-exec" {
+    command = "sudo openssl x509 -in ca.pem -out /usr/local/share/ca-certificates/ca.crt && sudo update-ca-certificates && sudo systemctl restart docker"
+  }
+}
+
+output "private_key_pem" {
+  value = tls_private_key.ca.private_key_pem
+  sensitive = true
+}
+
+output "private_key_algorithm" {
+  value = tls_private_key.ca.algorithm
+}
+
+output "public_key_pem" {
+  value = tls_self_signed_cert.ca.cert_pem
+}
+
+output "public_key_path" {
+  value = local_file.ca.filename
+}
---
+++ cluster/ca/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
