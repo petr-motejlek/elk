da5729dba661ea8fe522babba96d1f4ed08d041c
Petr Motejlek <petr@motejlek.net>
2021-05-13 18:43:55 +0200

    spin up the cluster nodes using Terraform+Vagrant

--- host/provision.sh
+++ host/provision.sh
@@ -7,10 +7,17 @@ export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | apt-key add -
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc | apt-key add -
+apt-add-repository "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+
 apt-get update
 apt-get install -y --no-install-recommends \
+  build-essential \
   git \
   "linux-headers-$( uname -r )" \
   ntp \
   rsync \
-  terraform
+  terraform \
+  vagrant \
+  virtualbox-6.1
