00c553b117e98966077fe4e4118c3e7a79d9d520
Petr Motejlek <petr@motejlek.net>
2021-05-13 18:56:47 +0200

    deploy MetalLB

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,3 +1,8 @@
+provider "helm" {
+  kubernetes {
+    config_path = module.k8s.kubeconfig_path
+  }
+}
 provider "kubernetes" {
   config_path = module.k8s.kubeconfig_path
 }
@@ -20,4 +25,10 @@ module "k8s" {
 
   dockerio_user  = var.dockerio_user
   dockerio_token = var.dockerio_token
+}
+
+module "metallb" {
+  depends_on = [
+  module.k8s]
+  source = "./metallb"
 }
