f9117c49eb27e4ec90e33090d9ce7491550a8451
Petr Motejlek <petr@motejlek.net>
2021-07-04 14:07:02 +0200

    use locals and variables in the CA module

--- cluster/main.tf
+++ cluster/main.tf
@@ -21,8 +21,18 @@ variable "dockerio_token" {
   sensitive = true
 }
 
+locals {
+  ca-common_name     = "elk-ca"
+  ca-valid-hours     = 24 * 365
+  ca-public_key-path = abspath("${path.module}/ca.pem")
+}
+
 module "ca" {
   source = "./ca"
+
+  ca-common_name     = local.ca-common_name
+  ca-valid-hours     = local.ca-valid-hours
+  ca-public_key-path = local.ca-public_key-path
 }
 
 module "k8s" {
@@ -32,8 +42,8 @@ module "k8s" {
 
   dockerio_user      = var.dockerio_user
   dockerio_token     = var.dockerio_token
-  ca_public_key_hash = module.ca.public_key_hash
-  ca_public_key_path = module.ca.public_key_path
+  ca_public_key_hash = module.ca.public_key-hash
+  ca_public_key_path = module.ca.public_key-path
 }
 
 module "metallb" {
@@ -60,9 +70,9 @@ module "registry" {
   module.ca]
   source = "./registry"
 
-  ca_private_key_pem       = module.ca.private_key_pem
-  ca_private_key_algorithm = module.ca.private_key_algorithm
-  ca_public_key_pem        = module.ca.public_key_pem
+  ca_private_key_pem       = module.ca.private_key-pem
+  ca_private_key_algorithm = module.ca.private_key-algorithm
+  ca_public_key_pem        = module.ca.public_key-pem
 
   storage_class = module.longhorn.storage_class
 }
